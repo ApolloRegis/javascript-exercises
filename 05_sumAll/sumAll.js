bc5347a5383defabbf0384ddecb0899dec0c301d
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-10 17:56:01 -0500

    MOVE RETURN(SUM)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,12 +2,13 @@ const sumAll = function(num1, num2) {
     let big = Math.max(num1, num2);
     let small = Math.min(num1, num2);
     let sum = big;
-
+  
     for (let i = small; i<big; i++){
         sum += i;
-        return sum;
     };
-};
+    
+    return(sum)
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
