c92e29106d19c8980354fd3eeb6ce83a5c718c05
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-10 17:21:10 -0500

    ADD MAIN FUNCTION BODY
    Loop through all numbers from the smallest to the largest.
    Add each iteration to the rolling sum starting with the largest number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,6 +3,10 @@ const sumAll = function() {
     let small = Math.min(num1, num2);
     let sum = big;
 
+    for (let i = small; i<big; i++){
+        sum += i;
+        return sum;
+    };
 };
 
 // Do not edit below this line
