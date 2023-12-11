e3f60a23f7782a6d5a3e308f292121020567c35a
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-10 19:52:38 -0500

    ADD QUALIFICATION FUNCTION
    Check all input parameters to verify that they are all positive
    numeric values.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,20 @@
 const sumAll = function(num1, num2) {
-    let big = Math.max(num1, num2);
-    let small = Math.min(num1, num2);
-    let sum = big;
-  
-    for (let i = small; i<big; i++){
-        sum += i;
-    };
-    
-    return(sum)
+    if (typeof num1 !== 'number' || isNaN(num1) || num1<0){
+        return "ERROR!";
+      } if (typeof num2 !== 'number' || isNaN(num2) || num2<0) {
+        return "ERROR!";
+      } else {
+        
+        let big = Math.max(num1, num2);
+        let small = Math.min(num1, num2);
+        let sum = big;
+      
+        for (let i = small; i<big; i++){
+            sum += i;
+        };
+      
+        return(sum);
+      };
   };
 
 // Do not edit below this line
