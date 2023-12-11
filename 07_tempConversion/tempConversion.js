7eda2ba49bc10ac0611ed2c84824236b2c8607a0
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-11 16:57:07 -0500

    F TO C CONVERSION
    Multiply parameter by 1.8, add 32, return new number with one decimal.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -4,7 +4,10 @@ const convertToCelsius = function(num) {
   return nwnum.toFixed(1); 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let nwnum = (num*1.8)+32;
+
+  return nwnum.toFixed(1);
 };
 
 // Do not edit below this line
