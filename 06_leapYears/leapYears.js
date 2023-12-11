70e5541b35531f559ce3bedc2ff3aa4f7caea88b
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-10 21:42:43 -0500

    FUNCTION MAIN BODY
    Evaluate num for whether it is divisible by 4, if so check
    to see if num is divisible by 100, if so check to see if
    num is divisible by 400.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
 const leapYears = function() {
-
+    if (num%4 === 0) {
+        if (num%100 === 0) {
+            if (num%400 === 0) {
+                return "true";
+            } else {
+                return "false";
+            };
+        } else {
+            return "true";
+        };
+    } else {
+        return "false";
+    };
 };
 
 // Do not edit below this line
