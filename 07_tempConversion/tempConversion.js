e70ce45d5585da8f35599613efbdd3b2b3cb0e43
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-11 17:11:21 -0500

    CONVERT RESULTS BACK INTO NUMBERS FROM STRINGS

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 const convertToCelsius = function(num) {
   let nwnum = (num-32)/1.8;
 
-  return nwnum.toFixed(1); 
+  return parseInt(nwnum.toFixed(1)); 
 };
 
 const convertToFahrenheit = function(num) {
   let nwnum = (num*1.8)+32;
 
-  return nwnum.toFixed(1);
+  return parseInt(nwnum.toFixed(1));
 };
 
 // Do not edit below this line
