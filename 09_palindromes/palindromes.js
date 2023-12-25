fd570e75b637aa053526d109158c5476cc7cb0ba
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-25 06:22:55 -0500

    Practice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let basicString = string.toLowerCase().replace(/[^a-z]/g, "");
+    let revStr = basicString.split("").reverse().join("");
 
+    if(basicString === revStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+console.log(palindromes('r3ace3car'));
 
 // Do not edit below this line
 module.exports = palindromes;
