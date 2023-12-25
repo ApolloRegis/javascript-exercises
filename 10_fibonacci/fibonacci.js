97116400c4d20676f38d9c65cfce51e3ba2c5ce8
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-25 09:33:56 -0500

    PRACTICE

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num === 'string') { 
+        num = parseInt(num); 
+    };
+        
+    if (num < 0) { 
+        return "OOPS";
+    };
+
+    let sequence = [0, 1];
+
+    for (let i = 2; i <= num; i++) { 
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    };
+
+    return sequence[num];
 
 };
+
+//console.log(fibonacci(15));
 
 // Do not edit below this line
 module.exports = fibonacci;
