442c0366824ac1d1c82a4c07507cf180ea104dba
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-26 17:00:29 -0500

    PRACTICE

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let personWithGreatestAge; 
+    let greatestAge = 0; 
+    
+    for (let i = 0; i < people.length; i++) { 
+        const person = people[i]; 
+        const currentYear = new Date().getFullYear();
+        let age; 
+        
+        if (person.yearOfDeath) {
+            age = person.yearOfDeath - person.yearOfBirth; 
+        } else { 
+            age = currentYear - person.yearOfBirth; 
+        }; 
+        
+        if (age > greatestAge) { 
+            greatestAge = age; 
+            personWithGreatestAge = person; 
+        }; 
+    }; 
+    
+    return personWithGreatestAge;
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
